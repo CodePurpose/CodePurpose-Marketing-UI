322e59f10fc4d560fe2954f5d7a3a6fccf403809
Emilio <em.francomartinez@gmail.com>
2019-06-26 16:04:27 -0600

    removed unnessary div from AboutUs, Careers, Discussion, and HireUs components.

--- src/component/Discussion.js
+++ src/component/Discussion.js
@@ -3,18 +3,14 @@ import { Container } from "../Styles/StyledComponents";
 
 const Discussion = () => (
   <Container style={{ textAlign: "center" }}>
-    <div>
-      <h3>THERE IS ALWAYS ROOM FOR DISCUSSION</h3>
-    </div>
-    <div>
-      <p>
-        Our objective in striving for identity equity is never to exclude an
-        employee, client, or anyone else. We are aspiring to start a
-        conversation for change first and foremost. We expect to make mistakes
-        because they are a benchmark of progress. But all progress is gradual,
-        and solutions are only found through an iterative approach.
-      </p>
-    </div>
+    <h3>THERE IS ALWAYS ROOM FOR DISCUSSION</h3>
+    <p>
+      Our objective in striving for identity equity is never to exclude an
+      employee, client, or anyone else. We are aspiring to start a conversation
+      for change first and foremost. We expect to make mistakes because they are
+      a benchmark of progress. But all progress is gradual, and solutions are
+      only found through an iterative approach.
+    </p>
   </Container>
 );
 
--- src/component/HireUs.js
+++ src/component/HireUs.js
@@ -3,19 +3,15 @@ import { Container } from "../Styles/StyledComponents";
 
 const HireUs = () => (
   <Container style={{ textAlign: "center" }}>
-    <div>
-      <h3>Are you looking to hire your own team of engineers?</h3>
-    </div>
-    <div>
-      <p>
-        Our solutions extend to more than just our custom software. CodePurpose
-        was founded on the principle of innovation and opportunity. We provide
-        top-quality engineers looking to fill your needs whether that means
-        internships, contract projects, or permanent solutions. Our vast pool of
-        engineers come from varied backgrounds but share the same objective of
-        providing innovation and superior builds.
-      </p>
-    </div>
+    <h3>Are you looking to hire your own team of engineers?</h3>
+    <p>
+      Our solutions extend to more than just our custom software. CodePurpose
+      was founded on the principle of innovation and opportunity. We provide
+      top-quality engineers looking to fill your needs whether that means
+      internships, contract projects, or permanent solutions. Our vast pool of
+      engineers come from varied backgrounds but share the same objective of
+      providing innovation and superior builds.
+    </p>
   </Container>
 );
 
